ad8acc2f44f29e558ad6b7d79d9715d705a16490
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-06-26 20:00:45 +0600

    start-work

---
+++ src/pages/Slick.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Slider from "react-slick";
+
+const Slick = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
+
+  return (
+    <div className="container">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Slick;
